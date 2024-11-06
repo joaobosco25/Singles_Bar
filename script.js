91ffb47c34c8089c07dfe0b22fe44a9968cdeae8
Joao Bosco Ferreira Duarte <joaobosco2097@hotmail.com>
2024-11-06 14:59:03 -0300

    fix: commit fixed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleMenu() {
     const menu = document.getElementById('menuLateral'); 
     menu.classList.toggle('active'); 
@@ -13,33 +12,12 @@ document.addEventListener('click', function(event) {
     }
 });
 
-=======
-// Função para alternar o menu lateral
-function toggleMenu() {
-    const menu = document.getElementById('menuLateral'); // Obtem o menu lateral
-    menu.classList.toggle('active'); // Alterna a classe active
-}
-
-// Fechar o menu ao clicar fora dele
-document.addEventListener('click', function(event) {
-    const menu = document.getElementById('menuLateral'); // Referência ao menu lateral
-    const menuIcon = document.querySelector('.menu-icon'); // Referência ao ícone do menu
-
-    // Verifica se o menu está ativo e se o clique foi fora do menu e do ícone
-    if (menu.classList.contains('active') && !menu.contains(event.target) && !menuIcon.contains(event.target)) {
-        menu.classList.remove('active'); // Fecha o menu
-    }
-});
-
-// Funções para o slideshow
->>>>>>> 861facf40c2b0650ec0eee72a9c7eab2754d5ff9
 let slideIndex = 0;
 const slides = document.querySelectorAll('.slide');
 showSlide(slideIndex);
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
-<<<<<<< HEAD
         slide.classList.remove('active', 'prev', 'next'); 
         if (i === index) {
             slide.classList.add('active'); 
@@ -47,20 +25,10 @@ function showSlide(index) {
             slide.classList.add('prev'); 
         } else if (i === (index + 1) % slides.length) {
             slide.classList.add('next'); 
-=======
-        slide.classList.remove('active', 'prev', 'next'); // Remove todas as classes
-        if (i === index) {
-            slide.classList.add('active'); // Ativa o slide atual
-        } else if (i === (index - 1 + slides.length) % slides.length) {
-            slide.classList.add('prev'); // Adiciona classe para o slide anterior
-        } else if (i === (index + 1) % slides.length) {
-            slide.classList.add('next'); // Adiciona classe para o slide seguinte
->>>>>>> 861facf40c2b0650ec0eee72a9c7eab2754d5ff9
         }
     });
 }
 
-function nextSlide() {
     slideIndex = (slideIndex + 1) % slides.length;
     showSlide(slideIndex);
 }
@@ -68,15 +36,6 @@ function nextSlide() {
 function prevSlide() {
     slideIndex = (slideIndex - 1 + slides.length) % slides.length;
 }
-
-<<<<<<< HEAD
-setInterval(nextSlide, 8000); 
-
-const modal = document.getElementById("modal");
-const abrirModalButton = document.getElementById("abrirModal");
-
-abrirModalButton.onclick = function() {
-    modal.style.display = "flex"; 
 }
 
 window.onclick = function(event) {
